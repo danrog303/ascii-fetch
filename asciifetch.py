19e5492c2a69f4b334cabc761d1ca67f9597036e
Daniel Rogowski <daniel.rogowski@onet.pl>
2022-01-03 16:44:43 +0100

    Add Argument Parser in main script file

--- asciifetch.py
+++ asciifetch.py
@@ -1,15 +1,15 @@
+import argparse
 from asciifetch.scrapers.category_scraper import CategoryScraper
 from asciifetch.scrapers.art_scraper import ArtScraper
 
 
 def main():
-    # Example 1
-    # Printing ASCII Art number 2 from https://www.asciiart.eu/animals/deer
-    print(ArtScraper("https://www.asciiart.eu/animals/deer").get_ascii_arts()[1])
-
-    # Example 2
-    # Printing all subcategories in https://www.asciiart.eu/animals
-    print(CategoryScraper("https://www.asciiart.eu/animals").get_categories())
+    parser = argparse.ArgumentParser(
+        description="Downloads and displays ASCII images in your terminal.",
+        epilog="https://github.com/danrog303/ascii-fetch"
+    )
+    parser.add_argument(nargs='*', dest="requested_path")
+    args = parser.parse_args()
 
 
 if __name__ == '__main__':
